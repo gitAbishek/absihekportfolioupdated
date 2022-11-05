15a8f0951c8c582d69d88987780ad6f871025f92
abishekGithub <abishekshah12315@gmail.com>
2022-11-05 13:53:38 +0545

    remove cv.pdf

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -2,7 +2,7 @@ import CV from "../../assets/CV.pdf"
 const CTA = () =>{
     return (
         <div className="cta">
-            <a href={CV} download className="btn">Download CV</a>
+            <a href="#" download className="btn">Download CV</a>
             <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
     )
